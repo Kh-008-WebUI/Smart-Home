67762aa1c187278c0c2e1a63d0b80b23e47cb6fe
Kaderimon <schweppes2006@online.ua>
2017-07-12 04:53:46 +0300

    saga device add, and builder notification

--- src/reducers/addDevice.reducer.js
+++ src/reducers/addDevice.reducer.js
@@ -1,10 +1,18 @@
-const devices = (state = [], action) => {
+const initialState = {
+  status:''
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_DEVICE':
-      return [...state, action.device];
+      return Object.assign({}, state, { status:'PENDING' });
+    case 'ADD_DEVICE_SUCCESS':
+      return Object.assign({}, state, { status:'DONE' });
+    case 'ADD_DEVICE_FAILURE':
+      return Object.assign({}, state, { status:'FAIL' });
     default:
       return state;
   }
 };
 
-export default devices;
+export default reducer;
