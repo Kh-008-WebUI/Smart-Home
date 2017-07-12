f9fbbf5ad1feb3484e9b9cec801eca4a23dd6fdd
Svetlana Semenova <kononenko.s.e@gmail.com>
2017-07-12 23:20:56 +0300

    change api export for notifications

--- src/api/notificationsApi.js
+++ src/api/notificationsApi.js
@@ -1,9 +1,12 @@
 import jsonNotifications from '../data/notifications.json';
 
-export default () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(jsonNotifications);
-    }, 1000);
-  });
+export default class notificationsApi {
+  static getNotifications () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(jsonNotifications);
+      }, 1000);
+    });
+  };
 };
+
--- src/sagas/notificationsSaga.js
+++ src/sagas/notificationsSaga.js
@@ -1,9 +1,9 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
-import Api from '../api/notificationsApi';
+import notificationsApi from '../api/notificationsApi';
 import { fetchNotificationsSuccess } from '../actions/loadNotifications.action';
 
 function* fetchNotifications () {
-  const notifications = yield call(Api);
+  const notifications = yield call(notificationsApi.getNotifications);
 
   yield put(fetchNotificationsSuccess(notifications));
 }
