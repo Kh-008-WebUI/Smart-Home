b6ecad815289115ea53fc8470617d36d6c1fb9c0
Giperboloid <kostyamol@gmail.com>
2017-07-13 17:47:16 +0300

    remove scroll

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,15 +4,13 @@ import builder from './builder.reducer';
 import devicesList from './devicesList.reducer';
 import { loadUsersReducer } from './users.reducer';
 import notificationsReducer from './notifications.reducer';
-import loadDevicesReducer from './loadDevicesReducer.js';
 
 const rootReducer = combineReducers({
   searchAndFilter,
   builder,
   devicesList,
   loadUsersReducer,
-  notificationsReducer,
-  loadDevicesReducer
+  notificationsReducer
 });
 
 export default rootReducer;
