4ae7a29f5a0dcc18b280cab6524c00b6daa1dab4
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-14 13:27:16 +0300

    add spinner to users list

--- src/components/DashboardContent/ListUsers.js
+++ src/components/DashboardContent/ListUsers.js
@@ -20,13 +20,13 @@ class ListUsers extends React.Component {
     const listAtHome = list.filter((item) => (item.home));
 
     if (list.length === 0) {
-      return (<section className='list-persons'>
-        <i className="fa fa-3x fa-spinner fa-spin"></i>
+      return (<section className='list-users'>
+        <i className='fa fa-3x fa-spinner fa-spin'></i>
       </section>);
     }
 
     return (
-      <section className='list-persons'>
+      <section className='list-users'>
         {listAtHome.map((item, key) => {
           return (
             <li key={item.id}>
