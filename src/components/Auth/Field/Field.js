95117fc9710d7a2898f69612fcba4a601272cb1d
Kaderimon <schweppes2006@online.ua>
2017-07-15 17:26:52 +0300

    login and register pages

---
+++ src/components/Auth/Field/Field.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export const Field = (props) => (
+  <div className="signup-field-group">
+    <label
+      htmlFor={ `${ props.name }` }
+      className="signup-form__label">
+      { props.name }
+    </label>
+    <input type="text"
+      name={ `${ props.name }` }
+      className="signup-form__input-field"/>
+    <p className="caption signup-form__caption">
+      { props.text }
+    </p>
+  </div>
+);
+
+Field.propTypes = {
+  name: PropTypes.string,
+  text: PropTypes.string
+};
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,73 +1,30 @@
 import React, { Component } from 'react';
+import { Header } from '../../components/Auth/Header/Header';
+import { Field } from '../../components/Auth/Field/Field';
+
 require('./Register.scss');
 
 export default class Register extends Component {
   render () {
     return (
       <div className="signup-container">
-        <div className="signup-header">
-          <i
-            className="fa fa-user-plus fa-3x signup-header__icon"
-            aria-hidden="true">
-          </i>
-          <h1 className="signup-header__title">Register
-            <span className="caption signup-header__caption">
-              Please enter your data to register.
-            </span>
-          </h1>
-        </div>
+        <Header
+          pic={'fa-user'}
+          title={'Register'}
+          text={'Please enter your data to register.'} />
         <form className="signup-form">
-          <div className="signup-field-group">
-            <label
-              htmlFor="username"
-              className="signup-form__label">
-              Username
-            </label>
-            <input type="text"
-              name="username"
-              className="signup-form__input-field"/>
-            <p className="caption signup-form__caption">
-              Your unique username to app
-            </p>
-          </div>
-          <div className="signup-field-group">
-            <label
-              htmlFor="password"
-              className="signup-form__label">
-              Password
-            </label>
-            <input type="text"
-              name="password"
-              className="signup-form__input-field"/>
-            <p className="caption signup-form__caption">
-              Your hard to guess password
-            </p>
-          </div>
-          <div className="signup-field-group">
-            <label
-              htmlFor="password-repeat"
-              className="signup-form__label">
-              Repeat Password
-            </label>
-            <input type="text"
-              name="password-repeat"
-              className="signup-form__input-field"/>
-            <p className="caption signup-form__caption">
-              Please repeat your pasword
-            </p>
-          </div>
-          <div className="signup-field-group">
-            <label
-              htmlFor="email"
-              className="signup-form__label">
-              Email Address</label>
-            <input type="email"
-              name="email"
-              className="signup-form__input-field"/>
-            <p className="caption signup-form__caption">
-              Your address email to contact
-            </p>
-          </div>
+          <Field
+            name="Username"
+            text={'Your unique username to app'}/>
+          <Field
+            name="Password"
+            text={'Your hard to guess password'}/>
+          <Field
+            name="Repeat-Password"
+            text={'Please repeat your pasword'}/>
+          <Field
+            name="Email"
+            text={'Your address email to contact'}/>
           <div className="signup-field-group signup-btn-group">
             <input
               type="button"
