75f045aaa72ccb9a772219f63771369125d4cc63
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-16 12:14:57 +0300

    redone Dashboard into stupid component

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,24 +4,14 @@ import ListDevices from
 import DashboardListUsers from
 '../../components/DashboardContent/ListUsers';
 import './Dashboard.scss';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
-class Dashboard extends React.Component {
-  constructor (props) {
-    super(props);
-  }
-  render () {
-    return (
-     <div className='dashboard-block'>
-        <div className='dashboard-devices-and-persons'>
-          <ListDevices />
-          <DashboardListUsers />
-        </div>
-      </div>
-    );
-  }
-}
+const Dashboard = () => (
+  <div className='dashboard-block'>
+    <div className='dashboard-devices-and-persons'>
+      <ListDevices />
+      <DashboardListUsers />
+    </div>
+  </div>
+);
 
 export default Dashboard;
-
