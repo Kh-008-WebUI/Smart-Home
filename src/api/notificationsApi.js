35ece523a351769d64d970b2361aef5e7e4fe657
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-18 10:42:48 +0300

    delete error testing

--- src/api/notificationsApi.js
+++ src/api/notificationsApi.js
@@ -3,7 +3,7 @@ import jsonNotifications from '../data/notifications.json';
 export const getNotifications = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject(jsonNotifications);
+      resolve(jsonNotifications);
     }, 1000);
   });
 };
