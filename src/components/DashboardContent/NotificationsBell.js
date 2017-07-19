88d99a51ed2721c304ae8629491912220d7ce557
rost <rost@ukr.net>
2017-07-19 09:51:42 +0300

    fixed bell and devices power

--- src/components/DashboardContent/NotificationsBell.js
+++ src/components/DashboardContent/NotificationsBell.js
@@ -19,18 +19,11 @@ class NotificationsBell extends React.Component {
 
   displayNotify = () => {
     if (this.props.loadNotifacationsStatus !== 'ERROR') {
-      this.bell.classList.add('notification-display');
-      document.addEventListener('click', this.hideNotify);
+      this.bell.classList.toggle('notification-display');
     }
-  }
-
-  hideNotify = () => {
-    this.bell.classList.remove('notification-display');
-    document.removeEventListener('click', this.hideNotify);
   }
   getNotify = (el) => {
     this.props.changeStatusNotification(el.target.id);
-    this.displayNotify();
   }
   render () {
     const listNotify = this.props.notifications;
