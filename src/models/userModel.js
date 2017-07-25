81258ef9d4fe97747432f6271922b60d1c7fff51
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-25 13:25:45 +0300

    remove server.js

--- src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userModel = new Schema({
-  name: String,
-  home: Boolean
-});
-
-module.exports = mongoose.model('User', userModel);
