5c5b6cc67befd0fca533d03166014f2a5d799c63
Mykyta Shupyliuk <schweppes2006@online.ua>
2017-08-01 11:17:27 +0300

    loginka (#124)
    * npm run prod version
    
    * added prod an dev versioning
    
    * let to const

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,6 +1,7 @@
+const production = 'production' === process.env.NODE_ENV;
 
 module.exports = (req, res, next) => {
-  if(!req.session.user){
+  if(!req.session.user && production){
     res.send(500);
   }else{
     next();    
