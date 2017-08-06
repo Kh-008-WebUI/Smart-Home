cd038647f20e8b06c98f0236fbba1399f70b93f9
Kaderimon <schweppes2006@online.ua>
2017-08-07 00:03:06 +0300

    center text and stuff

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LineChart from './LineChart/LineChart';
-import { ws } from '../../index';
 
 export default class Chart extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      calcWidth: 200,
-      data: []
+      calcWidth: 200
     };
   }
 
   componentDidMount () {
-    ws.onmessage = msg => {
-      const message = JSON.parse(msg.data);
-
-      if (message.type === 'chart') {
-        this.setState({
-          ...this.state,
-          data: message.data
-        });
-      }
-    };
     window.addEventListener('resize', this.setChildWidth);
     this.setChildWidth();
   }
@@ -44,8 +32,7 @@ export default class Chart extends React.Component {
           this.div = el;
         } }>
           <LineChart
-            width={this.state.calcWidth}
-            data={this.state.data}/>
+            width={this.state.calcWidth}/>
       </div>
     );
   }
--- src/components/DashboardContent/DashChart.js
+++ src/components/DashboardContent/DashChart.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './DashChart.scss';
 import Chart from '../../components/Chart/Chart';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ws } from '../../index';
 
-const DashChart = () => (
-  <div className='chart'>
-    <Chart />
-  </div>
-);
+class DashChart extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  componentDidMount () {
+    ws.onmessage = msg => {
+      const message = JSON.parse(msg.data);
 
-export default DashChart;
+      if (message.type === 'chart') {
+        const d = 1;
+      }
+    };
+  }
+  render () {
+    return (
+      <div className='chart'>
+        <h2 className="list-title">ChartMUTHAFUCKA</h2>
+        <Chart />
+      </div>
+    );
+  }
+}
+function mapStateToProps (store) {
+  return {
+  };
+}
+function mapDispatchToProps (dispatch) {
+  return {
+  };
+}
+
+DashChart.propTypes = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashChart);
