bababd4dde9e67750d7860d4fc96c9f76f2ff350
AnastasiiaHamaiunova <asikalove@yandex.ua>
2017-08-08 10:16:51 +0300

    Layout websocet

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -10,6 +10,9 @@ import {
   getLoggedUser,
   logout,
   clearLoginStatus } from '../../actions/auth.action';
+import {
+    fetchNotificationsRequest
+  } from '../../actions/notifications.action';
 import { Message } from '../../components/Message/Message';
 import { fetchAddNotifications } from '../../actions/notifications.action';
 import { updateChart } from '../../actions/chart.action';
@@ -33,7 +36,7 @@ class MainLayout extends Component {
     ws.onmessage = (msg)=>{
       webSocket(
         msg,
-        this.props.fetchAddNotifications,
+        this.props.getNotifications,
         this.props.updateChart);
     };
   }
@@ -78,7 +81,8 @@ function mapDispatchToProps (dispatch) {
     getLoggedUser: bindActionCreators(getLoggedUser, dispatch),
     clearLoginStatus: bindActionCreators(clearLoginStatus, dispatch),
     fetchAddNotifications: bindActionCreators(fetchAddNotifications, dispatch),
-    updateChart: bindActionCreators(updateChart, dispatch)
+    updateChart: bindActionCreators(updateChart, dispatch),
+    getNotifications: bindActionCreators(fetchNotificationsRequest, dispatch)
   };
 }
 
@@ -91,6 +95,7 @@ MainLayout.propTypes = {
   status: PropTypes.string,
   clearLoginStatus: PropTypes.func,
   fetchAddNotifications: PropTypes.func,
-  updateChart: PropTypes.func
+  updateChart: PropTypes.func,
+  getNotifications: PropTypes.func
 };
 export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
