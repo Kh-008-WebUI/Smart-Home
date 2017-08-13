7fa96aeac90fc144355262cb132b7fc3e8748c60
Kaderimon <schweppes2006@online.ua>
2017-08-14 01:56:11 +0300

    server promise in routes

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -3,33 +3,31 @@ const locationRouter = express.Router();
 const Location = require('../models/location');
 
 locationRouter.route('/').get((req, res) => {
-  Location.find((err, locations) => {
-    if (err) {
+  Location.find()
+    .then( locations => {
+      res.json(locations);
+    })
+    .catch( err => {
       res.status(500).send({
         status: 'error',
         text: 'Something went wrong, try again later.'
       });
-
-      return;
-    }
-
-    res.json(locations);
-  });
+    });
 });
 
 locationRouter.route('/').post((req, res) => {
   const location = req.body.location;
-console.log(req.body);
-  Location.create({value: location, label:location}, (err, location) => {
-    if (err) {
+
+  Location.create({value: location, label:location})
+    .then( location => {
+      res.json(location);
+    })
+    .catch( err => {
       res.status(500).send({
         status: 'error',
         text: 'Could not add the location.'
       });
-    } else {
-      res.json(location);
-    }
-  });
+    });
 });
 
 module.exports = locationRouter;
