dec7bbcd193cb2ddcf6ba8f5ed54d16b82f1b50f
AnastasiiaHamaiunova <asikalove@yandex.ua>
2017-08-23 17:11:07 +0300

    test for delete device reducer

--- src/tests/devicesList.reducer.test.js
+++ src/tests/devicesList.reducer.test.js
@@ -1,5 +1,5 @@
 import { devicesList } from '../reducers/devicesList.reducer';
-import { LOAD_DEVICE_SUCCESS } from '../constants/index';
+import { LOAD_DEVICE_SUCCESS, DELETE_DEVICE_SUCCESS } from '../constants/index';
 
 const initialState = {
   filterOption: 'all',
@@ -10,6 +10,66 @@ const initialState = {
   errorText: '',
   errorStatus: 0
 };
+const devices = [{
+  _id : '599c8094258df03990fae071',
+  name : 'Device Test',
+  location : 'Parking',
+  status : true,
+  createdDate : 'August 22, 2017',
+  createdBy : 'Bruce',
+  items : [
+    {
+      description : 'Toggle',
+      data : false,
+      name : 'Toggle'
+    },
+    {
+      description : 'Value',
+      data : 'hello there',
+      name : 'Value'
+    },
+    {
+      description : 50,
+      params : {
+        'maxValue' : 100,
+        'minValue' : 0
+      },
+      data : 50,
+      name : 'Range'
+    }
+  ],
+  views : 1
+},
+{
+_id : '589c8094258df03990fae071',
+name : 'Device Test',
+location : 'Parking',
+status : true,
+createdDate : 'August 22, 2017',
+createdBy : 'Bruce',
+items : [
+  {
+    description : 'Toggle',
+    data : false,
+    name : 'Toggle'
+  },
+  {
+    description : 'Value',
+    data : 'hello there',
+    name : 'Value'
+  },
+  {
+    description : 50,
+    params : {
+      'maxValue' : 100,
+      'minValue' : 0
+    },
+    data : 50,
+    name : 'Range'
+  }
+],
+views : 1
+}]
 
 const device =
 {
@@ -59,5 +119,19 @@ describe('DeviceList Reducer', () => {
         device
       }
     )).toEqual(expectedState);
+  });
+  it('delete device success', () => {
+    let expectedState = initialState;
+    expectedState.device = device;
+
+    let device = devices.filter((item) => {
+      return item._id !== '599c8094258df03990fae071'});
+
+    expect(devicesList('599c8094258df03990fae071',
+      {
+        type: DELETE_DEVICE_SUCCESS,
+        device
+      }
+    )).toEqual(device);
   })
 });
