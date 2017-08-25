4483fef22456529c9f9ba39fcf554ee543abef49
Kaderimon <schweppes2006@online.ua>
2017-08-25 12:48:27 +0300

    chat styles

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -33,7 +33,7 @@ class MainLayout extends Component {
     window.dispatchEvent(new Event('resize'));
   }
   componentDidMount () {
-    ws = new WebSocket(`ws://${config.origin}/`);
+    ws = new WebSocket(`wss://${config.origin}/`);
     ws.onmessage = (msg) => {
       const message = JSON.parse(msg.data);
 
