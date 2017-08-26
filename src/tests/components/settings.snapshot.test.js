bc88f74d72331b231720a811ca3410b419383057
AnastasiiaHamaiunova <hamaiunova.anastasiia@gmail.com>
2017-08-26 19:25:13 +0300

    1

--- src/tests/components/settings.snapshot.test.js
+++ src/tests/components/settings.snapshot.test.js
@@ -3,48 +3,39 @@ import ToggleSettings from '../../components/ToggleSettings/ToggleSettings';
 import SelectLocation from '../../components/SelectLocation/SelectLocation';
 import RangeSettings from '../../components/RangeSettings/Range';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 const locations = [
   {
-  "_id": {
-        "$oid": "59940beedfa2e10840ef5eac"
-    },
-    "value": "kitchen",
-    "label": "Kitchen",
-    "__v": 0
-},
-{
-    "_id": {
-        "$oid": "59940bf3dfa2e10840ef5ead"
-    },
-    "value": "roof",
-    "label": "Roof",
-    "__v": 0
-},
-{
-    "_id": {
-        "$oid": "599413dd9a6f1117dc4fe6a2"
-    },
-    "value": "parking",
-    "label": "Parking",
-    "__v": 0
-}
+    _id:'59940beedfa2e10840ef5eac',
+    value: 'kitchen',
+    label: 'Kitchen',
+  },
+  {
+    _id:'59940bf3dfa2e10840ef5ead',
+    value: 'roof',
+    label: 'Roof'
+  },
+  {
+    _id: '599413dd9a6f1117dc4fe6a2',
+    value: 'parking',
+    label: 'Parking'
+  }
 ];
 
 describe('Settings UI test', () => {
-it('ToggleSettings', () => {
-  const component = renderer.create(
-      <ToggleSettings
-              setItemValue={jest.fn()}
-              onChange={jest.fn()}/>
-  );
+  it('ToggleSettings', () => {
+    const toggle = shallow(
+      <ToggleSettings />
+    );
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+    const rendered = toggle.find('input');
 
-it('SelectLocation', () => {
-  const component = renderer.create(
+    expect(rendered.html().indexOf('checked={false}')).toEqual(-1);
+  });
+
+  it('SelectLocation', () => {
+    const component = renderer.create(
       <SelectLocation
             selectLocation={jest.array}
             locations= {locations}
@@ -53,20 +44,18 @@ it('SelectLocation', () => {
             defaultLocation='Kitchen'
             deviceExistInLocation={jest.fn()}
             deviceInLocation={jest.array}/>
-  );
+    );
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it('RangeSettings', () => {
-  const component = renderer.create(
-      <RangeSettings
-            setItemValue={jest.fn()}/>
-  );
+  it('RangeSettings', () => {
+    const component = renderer.create(
+      <RangeSettings/>
+    );
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-})
-
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
